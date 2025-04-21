041ed6e0b55db2b9b9c330a44ac44170ec2f4fab
D7-57 <10665@holbertonstudents.com>
2025-04-21 16:39:54 +0000

    T

--- main.c
+++ main.c
@@ -1,58 +1,67 @@
 #include "shell.h"
 
+/**
+ * main - Entry point for the simple shell
+ * @argc: Argument count
+ * @argv: Argument vector
+ *
+ * Return: Always 0
+ */
 int main(int argc, char **argv)
 {
-    char *line = NULL;
-    char **args = NULL;
-    size_t len = 0;
-    ssize_t read;
-    pid_t pid;
-    int status;
+	char *line = NULL;
+	size_t len = 0;
+	ssize_t read;
+	char **args;
+	pid_t pid;
+	int status;
 
-    (void)argc;
+	(void)argc;
 
-    while (1)
-    {
-        if (isatty(STDIN_FILENO))
-            write(STDOUT_FILENO, "#cisfun$ ", 9);
+	while (1)
+	{
+		if (isatty(STDIN_FILENO))
+			write(STDOUT_FILENO, "#cisfun$ ", 9);
 
-        read = getline(&line, &len, stdin);
+		read = getline(&line, &len, stdin);
+		if (read == -1)
+		{
+			if (isatty(STDIN_FILENO))
+				write(STDOUT_FILENO, "\n", 1);
+			break;
+		}
 
-        if (read == -1)  /* Ctrl+D (EOF) */
-        {
-            if (isatty(STDIN_FILENO))
-                write(STDOUT_FILENO, "\n", 1);
-            break;
-        }
+		if (line[read - 1] == '\n')
+			line[read - 1] = '\0';
 
-        if (line[read - 1] == '\n')  /* remove newline */
-            line[read - 1] = '\0';
+		args = parse_line(line);
+		if (args == NULL || args[0] == NULL)
+		{
+			free_tokens(args);
+			continue;
+		}
 
-        args = parse_line(line);
-        if (args == NULL || args[0] == NULL)
-            continue;
+		pid = fork();
+		if (pid == 0)
+		{
+			if (execve(args[0], args, environ) == -1)
+			{
+				perror(argv[0]);
+				exit(EXIT_FAILURE);
+			}
+		}
+		else if (pid > 0)
+		{
+			wait(&status);
+		}
+		else
+		{
+			perror("fork");
+		}
 
-        pid = fork();
-        if (pid == 0)
-        {
-            if (execve(args[0], args, environ) == -1)
-            {
-                perror(argv[0]);
-                exit(EXIT_FAILURE);
-            }
-        }
-        else if (pid > 0)
-        {
-            wait(&status);
-        }
-        else
-        {
-            perror("fork");
-        }
+		free_tokens(args);
+	}
 
-        free(args);
-    }
-
-    free(line);
-    return 0;
+	free(line);
+	return (0);
 }
--- parse_line.c
+++ parse_line.c
@@ -1,44 +1,52 @@
 #include "shell.h"
 
 /**
- * parse_line - splits a line into arguments
- * @line: the input line
+ * parse_line - Splits a line into tokens (arguments)
+ * @line: Input line to parse
  *
- * Return: an array of strings (arguments)
+ * Return: Array of strings (tokens), or NULL on failure
  */
 char **parse_line(char *line)
 {
-    char **tokens = NULL;
-    char *token;
-    int i = 0;
-    int bufsize = 64;
+	char **tokens;
+	char *token;
+	int i = 0, bufsize = 64;
 
-    tokens = malloc(sizeof(char *) * bufsize);
-    if (!tokens)
-    {
-        perror("malloc");
-        exit(EXIT_FAILURE);
-    }
+	tokens = malloc(sizeof(char *) * bufsize);
+	if (tokens == NULL)
+	{
+		perror("malloc");
+		exit(EXIT_FAILURE);
+	}
 
-    token = strtok(line, " \t\r\n");
-    while (token != NULL)
-    {
-        tokens[i++] = token;
+	token = strtok(line, " \t\r\n");
+	while (token != NULL)
+	{
+		tokens[i++] = token;
 
-        if (i >= bufsize)
-        {
-            bufsize += 64;
-            tokens = realloc(tokens, sizeof(char *) * bufsize);
-            if (!tokens)
-            {
-                perror("realloc");
-                exit(EXIT_FAILURE);
-            }
-        }
+		if (i >= bufsize)
+		{
+			bufsize += 64;
+			tokens = realloc(tokens, sizeof(char *) * bufsize);
+			if (tokens == NULL)
+			{
+				perror("realloc");
+				exit(EXIT_FAILURE);
+			}
+		}
 
-        token = strtok(NULL, " \t\r\n");
-    }
+		token = strtok(NULL, " \t\r\n");
+	}
+	tokens[i] = NULL;
 
-    tokens[i] = NULL;
-    return tokens;
+	return (tokens);
+}
+
+/**
+ * free_tokens - Frees memory allocated for tokens array
+ * @tokens: Null-terminated array of strings
+ */
+void free_tokens(char **tokens)
+{
+	free(tokens);
 }
