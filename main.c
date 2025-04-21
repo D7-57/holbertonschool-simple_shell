510eff0f7e8f82e24eb542a767f95fa2e986714c
D7-57 <10665@holbertonstudents.com>
2025-04-21 16:44:23 +0000

    T

--- main.c
+++ main.c
@@ -1,7 +1,64 @@
 #include "shell.h"
 
 /**
- * main - Entry point for the simple shell
+ * display_prompt - Displays shell prompt if interactive
+ */
+void display_prompt(void)
+{
+	if (isatty(STDIN_FILENO))
+		write(STDOUT_FILENO, "#cisfun$ ", 9);
+}
+
+/**
+ * handle_input - Reads input from stdin
+ * @line: Pointer to buffer
+ * @len: Pointer to length
+ *
+ * Return: Read size or -1 on EOF
+ */
+ssize_t handle_input(char **line, size_t *len)
+{
+	ssize_t read;
+
+	read = getline(line, len, stdin);
+	if (read == -1 && isatty(STDIN_FILENO))
+		write(STDOUT_FILENO, "\n", 1);
+	if (read > 0 && (*line)[read - 1] == '\n')
+		(*line)[read - 1] = '\0';
+	return (read);
+}
+
+/**
+ * execute_command - Forks and executes a command
+ * @args: Tokenized command arguments
+ * @argv: Argument vector for error messages
+ */
+void execute_command(char **args, char **argv)
+{
+	pid_t pid;
+	int status;
+
+	pid = fork();
+	if (pid == 0)
+	{
+		if (execve(args[0], args, environ) == -1)
+		{
+			perror(argv[0]);
+			exit(EXIT_FAILURE);
+		}
+	}
+	else if (pid > 0)
+	{
+		wait(&status);
+	}
+	else
+	{
+		perror("fork");
+	}
+}
+
+/**
+ * main - Entry point for the shell
  * @argc: Argument count
  * @argv: Argument vector
  *
@@ -13,26 +70,15 @@ int main(int argc, char **argv)
 	size_t len = 0;
 	ssize_t read;
 	char **args;
-	pid_t pid;
-	int status;
 
 	(void)argc;
 
 	while (1)
 	{
-		if (isatty(STDIN_FILENO))
-			write(STDOUT_FILENO, "#cisfun$ ", 9);
-
-		read = getline(&line, &len, stdin);
+		display_prompt();
+		read = handle_input(&line, &len);
 		if (read == -1)
-		{
-			if (isatty(STDIN_FILENO))
-				write(STDOUT_FILENO, "\n", 1);
 			break;
-		}
-
-		if (line[read - 1] == '\n')
-			line[read - 1] = '\0';
 
 		args = parse_line(line);
 		if (args == NULL || args[0] == NULL)
@@ -41,24 +87,7 @@ int main(int argc, char **argv)
 			continue;
 		}
 
-		pid = fork();
-		if (pid == 0)
-		{
-			if (execve(args[0], args, environ) == -1)
-			{
-				perror(argv[0]);
-				exit(EXIT_FAILURE);
-			}
-		}
-		else if (pid > 0)
-		{
-			wait(&status);
-		}
-		else
-		{
-			perror("fork");
-		}
-
+		execute_command(args, argv);
 		free_tokens(args);
 	}
 
